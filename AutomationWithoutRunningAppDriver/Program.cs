50ddba94566b7507f7c600d5fd698b8636b188b8
felipecapelli <felipecapelli_uru@hotmail.com>
2022-05-03 23:05:50 -0300

    First Lecture

---
+++ AutomationWithoutRunningAppDriver/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationWithoutRunningAppDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appiumLocalService = new AppiumServiceBuilder().UsingPort(4723).WithLogFile(new FileInfo(@"C:\Users\felip\source\repos\RunNotePad\TestingLog.txt")).Build();
+            appiumLocalService.Start();
+            AppiumOptions ao = new AppiumOptions();
+            ao.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+
+            WindowsDriver<WindowsElement> session = new WindowsDriver<WindowsElement>(appiumLocalService, ao);
+
+            session.Quit();
+        }
+    }
+}
